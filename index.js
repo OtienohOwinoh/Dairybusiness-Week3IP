59dada8ffa764adb7831c546800a4505ed178e71
OtienohOwinoh <coowinoh@gmail.com>
2022-07-19 15:42:33 +0300

    js styling

---
+++ index.js
@@ -0,0 +1,21 @@
+function produceReport(){
+
+    event.preventDefault();
+
+    let data= [];
+
+    data[0] = data["shed_a"] = parseInt(document.getElementById("shed_a").value);
+    data[1] = data["shed_b"] = parseInt(document.getElementById("shed_b").value);
+    data[2] = data["shed_c"] = parseInt(document.getElementById("shed_c").value);
+    data[3] = data["shed_d"] = parseInt(document.getElementById("shed_d").value);
+
+
+
+
+
+
+
+
+
+
+}
